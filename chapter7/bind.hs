3e77fbf2faa38af12181851d1b0d70b73262a321
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2025-01-29 03:25:32 +0800

    ch7: bind

---
+++ chapter7/bind.hs
@@ -0,0 +1,7 @@
+bindExp :: Integer -> String
+bindExp x =
+    let x = 10; y = 5
+         in "the integer was: "
+                ++ show x
+                ++ " and y was: "
+                ++ show y
